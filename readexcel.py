e488cb6dad5487eb3d1b57391d9a4b5ca468ce2b
Al-Zuji <msyahmi9610@gmail.com>
2024-08-08 01:37:20 +0800

    Update readexcel.py

--- readexcel.py
+++ readexcel.py
@@ -2,7 +2,7 @@
 # excel-python.py
 from openpyxl import Workbook
 from time import strftime
-Wb_test = Workbook("20240110.xlsx")
+Wb_test = Workbook("/YOUR/FILE/PATH.xlsx")
 
 # Select the active worksheet
 ws = Wb_test.active
@@ -18,4 +18,4 @@ print("date:",date_time)
 ws['A1'] = no
 ws['B1'] = date_time
 ws['C1'] = time
-Wb_test.save('20240110')
+Wb_test.save("/YOUR/FILE/PATH.xlsx")
